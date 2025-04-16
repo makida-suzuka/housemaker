8511be07c52d26fb401a104afc392fbd00bc890c
makida-suzuka <s-makida@jinso.co.jp>
2025-04-16 14:41:30 +0900

    [add]ハンバーガーメニュー実装

--- main.js
+++ main.js
@@ -1,17 +1,19 @@
-window.onload = function(){
-    let fadeInTarget = document.querySelectorAll('.fade-in');
-    window.addEventListener('scroll', () => {
-      for (let i = 0; i < fadeInTarget.length; i++){
-        const rect = fadeInTarget[i].getBoundingClientRect().top;
-        console.log('target要素の位置' + rect);
-        const scroll = window.pageYOffset || document.documentElement.scrollTop;
-        console.log('スクロール' + scroll);
-        const offset = rect + scroll;
-        const windowHeight = window.innerHeight; // 現在のブラウザの高さ
-        if (scroll > offset - windowHeight + 150) {
-          fadeInTarget[i].classList.add('scroll-in');
-        }
-      }
+window.addEventListener('load', function(){
+    const ham = document.querySelector('#js-hamburger'); //js-hamburgerの要素を取得し、変数hamに格納
+    const nav = document.querySelector('#js-nav'); //js-navの要素を取得し、変数navに格納
+    const overlay = this.document.querySelector('#overlay');
+    
+    ham.addEventListener('click', function () { //ハンバーガーメニューをクリックしたら
+        ham.classList.toggle('active'); // ハンバーガーメニューにactiveクラスを付け外し
+        nav.classList.toggle('active'); // ナビゲーションメニューにactiveクラスを付け外し
+        overlay.classList.toggle('active');
+        console.log('ok!'); // コンソール画面でokというメッセージが出る
     });
-};
+    overlay.addEventListener('click', () => { //オーバーレイをクリックしても閉じる
+        ham.classList.toggle('active'); // ハンバーガーメニューにactiveクラスを付け外し
+        nav.classList.toggle('active'); // ナビゲーションメニューにactiveクラスを付け外し
+        overlay.classList.toggle('active');
+        console.log('good!'); // コンソール画面でgood!というメッセージが出る
+      });
+})
 
